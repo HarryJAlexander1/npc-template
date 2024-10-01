d5b098c0c29080b1d40207e8b2837283367c81f4
Harry Alexander <harryalexander1499@gmail.com>
2024-10-01 11:38:21 +0100

    Interpolate vertices

--- npc_template/Assets/Scripts/ComputeGraph.cs
+++ npc_template/Assets/Scripts/ComputeGraph.cs
@@ -1,21 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
-
 public class ComputeGraph : MonoBehaviour
 {
     public List<GameObject> GroundSegments;
+    public GameObject Ground;
+    private Vector3[] GraphVertices;
     // Start is called before the first frame update
     void Start()
     {
-        var output = GetGroundTopVertices(GroundSegments);
+        Vector3[] temp = GetGroundTopVertices(GroundSegments);
+        GraphVertices = InterpolateVertices(temp, 8);
+    }
 
-        foreach (var item in output) 
+    void OnDrawGizmos()
+    { 
+        foreach (var vertex in GraphVertices) 
         {
-            Debug.Log(item);
-        }
-
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(vertex, 0.25f);
+        }     
     }
 
     // Update is called once per frame
@@ -40,6 +45,60 @@ public class ComputeGraph : MonoBehaviour
             }
         }
         return groundMeshVertices.Where(x => x.y == (transform.localScale.y * 0.5f)).ToArray();
+    }
+
+    private Vector3[] InterpolateVertices(Vector3[] vertices, int divisions)
+    {
+        List<Vector3> interpolatedVertices = new List<Vector3>();
+
+        for (int i = 0; i < vertices.Length - 4; i += 4) // each segment will always have 4 'surface' vertices
+        {
+            float minX = Mathf.Infinity;
+            float maxX = -Mathf.Infinity;
+            float minZ = Mathf.Infinity;
+            float maxZ = -Mathf.Infinity;
+
+            for (int j = i; j < i + 4; j++) // find min and max x and z coordinates per 4 vertices
+            {
+                if (vertices[j].x < minX)
+                    minX = vertices[j].x;
+                if (vertices[j].x > maxX)
+                    maxX = vertices[j].x;
+                if (vertices[j].z < minZ)
+                    minZ = vertices[j].z;
+                if (vertices[j].z > maxZ)
+                    maxZ = vertices[j].z;
+            }
+
+            float xInterval = (maxX - minX) / divisions;
+            float zInterval = (maxZ - minZ) / divisions;
+
+            for (int divX = 0; divX <= divisions; divX++) 
+            {
+                for (int divZ = 0; divZ <= divisions; divZ++) 
+                {
+                    Vector3 v = new Vector3(minX + (xInterval * divX), transform.localScale.y * 0.5f, minZ + (zInterval * divZ));
+                    // Debug.Log($"Generated vertex pos= {v}");
+                    if (!ContainsSimilarVertex(interpolatedVertices, v, 0.00001f))
+                    {
+                        interpolatedVertices.Add(v);
+                    }
+                }            
+            }
+        }
+        return interpolatedVertices.ToArray();
+    }
+
+    private bool ContainsSimilarVertex(List<Vector3> vertices, Vector3 newVertex, float tolerance)
+    {
+        foreach (var vertex in vertices)
+        {
+            if (Vector3.Distance(vertex, newVertex) < tolerance)
+            {
+                return true;  // Similar vertex found
+            }
+        }
+        return false;
     }
 
     private class Graph 
